4beda30f184e8cd93706a7f8bf02bcd2889e538f
Jet <dominicanes@inbox.ru>
2017-02-11 15:59:23 +0300

    Add constants.h (storage)

---
+++ constants.h
@@ -0,0 +1,20 @@
+//
+// Created by jet on 11.02.17.
+//
+
+#ifndef PICALC_CONSTANTS_H
+#define PICALC_CONSTANTS_H
+
+// TODO: change to precompiled header
+#include <array>
+
+#define LIGHT_SPEED 1
+
+#define ELECTRON_MASS 1
+#define ELECTRON_CHARGE 1
+
+#define NUMBER_OF_DIMENSIONS
+
+typedef std::array<double, NUMBER_OF_DIMENSIONS> Vector;
+
+#endif //PICALC_CONSTANTS_H
