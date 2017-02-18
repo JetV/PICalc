dce7201b1f7c47ec1cfa0c5a9fd0d4ac51f319d9
Jet <dominicanes@inbox.ru>
2017-02-18 19:22:37 +0300

    TODO constant for dimensions

--- constants.h
+++ constants.h
@@ -13,7 +13,7 @@
 #define ELECTRON_MASS 1
 #define ELECTRON_CHARGE 1
 
-#define NUMBER_OF_DIMENSIONS
+#define NUMBER_OF_DIMENSIONS 3 // TODO: Избавиться от константы, задавать в последствии в динамике, через методы vector
 
 typedef std::array<double, NUMBER_OF_DIMENSIONS> Vector;
 
