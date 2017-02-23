37dc1f5e822c5cc31f0e0b3ad89d02db71b96b37
Jet <dominicanes@inbox.ru>
2017-02-23 19:45:35 +0300

    Add node operators and node iterator, vertical pair nodes indexes calculate for grid

--- Grid.cpp
+++ Grid.cpp
@@ -11,23 +11,71 @@ Grid::Grid(Vector sizes, std::array<size_t, NUMBER_OF_DIMENSIONS> sizes_in_nodes
   size_t nodes_count = 1;
   size_t cells_count = 0;
 
-  for(auto& nodes_by_axis_count : sizes_in_nodes)
+  // Цикл по размерам во всех направлениях для пространственного объема
+  for(size_t i = 0; i < NUMBER_OF_DIMENSIONS; ++i)
   {
-    if(nodes_by_axis_count == 0)
+    // Проверка на некорректность значения ячеек
+    if(sizes_in_nodes[i] == 0)
     {
-      nodes_by_axis_count = 1;
+      sizes_in_nodes[i] = 1;
     }
 
-    nodes_count *= nodes_by_axis_count;
+    _sizes_in_cells[i] = 1;
 
-    if(nodes_by_axis_count != 1)
+    nodes_count *= sizes_in_nodes[i];
+    // Домножаем общее количество ячеек, домножаем на "число узлов-1", либо ничего не делаем
+    if(sizes_in_nodes[i] != 1)
     {
-      cells_count *= nodes_by_axis_count - 1;
+      _sizes_in_cells[i] = sizes_in_nodes[i] - 1;
+      cells_count *= _sizes_in_cells[i];
     }
   }
 
+  // Линейный массив узлов
   _nodes = std::vector<Node>(nodes_count);
   _cells = std::vector<Node>(cells_count);
+}
+
+Grid::Node& Grid::get_node(Vector_of_indexes indexes)
+{
+  // Напоминание: массив всех узлов линеен
+  size_t node_id = 0;
+  // Проход по всем измерениям от последнего
+  for(size_t i = NUMBER_OF_DIMENSIONS -1 ; i >= 0; --i)
+  {
+    // Получаем число узлов в слое
+    // Индекс по данной оси
+    size_t nodes_in_layer_count = indexes[i];
+    // Объём слоя под этим индексом (измеренный в узлах)
+    for(size_t j = i; j < NUMBER_OF_DIMENSIONS; ++j)
+    {
+      nodes_in_layer_count *= _sizes_in_nodes[j];
+    }
+    // Добавляем к общему индексу объем слоя по данной оси
+    node_id += nodes_in_layer_count;
+  }
+  return _nodes[node_id];
+}
+
+Grid::Node& Grid::get_cell(Vector_of_indexes indexes)
+{
+  // Напоминание: массив всех ячеек линеен
+  size_t cell_id = 0;
+  // Проход по всем измерениям от последнего
+  for(size_t i = NUMBER_OF_DIMENSIONS -1 ; i >= 0; --i)
+  {
+    // Получаем число ячеек в слое
+    // Индекс по данной оси
+    size_t cells_in_layer_count = indexes[i];
+    // Объём слоя под этим индексом (измеренный в ячейках)
+    for(size_t j = i; j < NUMBER_OF_DIMENSIONS; ++j)
+    {
+      cells_in_layer_count *= _sizes_in_cells[j];
+    }
+    // Добавляем к общему индексу объем слоя по данной оси
+    cell_id += cells_in_layer_count;
+  }
+  return _cells[cell_id];
 }
 
 Grid::Node::Node() :
@@ -37,3 +85,61 @@ Grid::Node::Node() :
   _E_Field.fill(0.0);
   _B_Field.fill(0.0);
 }
+
+// Check template! TODO: complete
+Vertical_pair_nodes_iterator& Grid::Vertical_pair_nodes_iterator::move(const std::array<int, NUMBER_OF_DIMENSIONS>& offset)
+{
+  // _indexes += offset;
+
+}
+
+Grid::Vertical_pair_nodes_iterator& Grid::Vertical_pair_nodes_iterator::operator += (Vertical_pair_nodes_iterator& iterator, int offset)
+{
+  iterator._index += offset;
+  iterator._bottom_node += offset;
+  iterator._top_node += offset;
+}
+
+Grid::Node& Grid::Vertical_pair_nodes_iterator::get_bottom_node()
+{
+  return *_bottom_node;
+}
+
+Grid::Node& Grid::Vertical_pair_nodes_iterator::get_top_node()
+{
+  return *_top_node;
+}
+
+Grid::Vertical_pair_nodes_iterator::Vertical_pair_nodes_iterator(size_t index, Node* bottom_node, Node* top_node) :
+  _index(index),
+  _bottom_node(bottom_node),
+  _top_node(top_node)
+{
+}
+
+Grid::Vertical_pair_nodes_iterator Grid::get_vertical_pair_nodes_iterator(Vector_of_indexes indexes)
+{
+  // Напоминание: массив всех узлов линеен
+  size_t node_id = 0;
+  // Проход по всем измерениям от последнего
+  for(size_t i = NUMBER_OF_DIMENSIONS -1 ; i >= 0; --i)
+  {
+    // Получаем число узлов в слое
+    // Индекс по данной оси
+    size_t nodes_in_layer_count = indexes[i];
+    // Объём слоя под этим индексом (измеренный в узлах)
+    for(size_t j = i; j < NUMBER_OF_DIMENSIONS; ++j)
+    {
+      nodes_in_layer_count *= _sizes_in_nodes[j];
+    }
+    // Добавляем к общему индексу объем слоя по данной оси
+    node_id += nodes_in_layer_count;
+  }
+
+  // принимает текущий узел (id), указатель на текущий узел, указатель на узел выше в вертикальной паре - это не работает для одномерного случая TODO:
+  return Vertical_pair_nodes_iterator(node_id, &_nodes[node_id], &_nodes[node_id + _sizes_in_nodes[NUMBER_OF_DIMENSIONS-2]]);
+}
+
+
+
+
--- Grid.h
+++ Grid.h
@@ -8,13 +8,20 @@
 
 #include <vector>
 #include <cstdio>
-#include "constants.h"
+#include "math_functions.h"
 
 class Grid {
 
   public:
-    Grid(Vector sizes, std::array<size_t, NUMBER_OF_DIMENSIONS> sizes_in_nodes);
+    Grid(Vector sizes, Vector_of_indexes sizes_in_nodes);
     ~Grid();
+
+    Node& get_node(Vector_of_indexes /*индекс элемента по оси*/indexes);
+    Node& get_cell(Vector_of_indexes indexes);
+
+    // создание и возвращение итератора вертикальных пар узлов
+    Vertical_pair_nodes_iterator get_vertical_pair_nodes_iterator(Vector_of_indexes indexes);
+
 
   private:
     struct Node
@@ -27,10 +34,41 @@ class Grid {
       unsigned double _dencity;
     };
 
+    struct Vertical_pair_nodes_iterator
+    {
+      // итератор ребра между вертикальной парой узлов
+      Vertical_pair_nodes_iterator(size_t index, Node* bottom_node, Node* top_node);
+
+      // получение ссылки на нижний в вертикальной паре узел
+      Node& get_bottom_node();
+      // получение ссылки на верхний в вертикальной паре узел
+      Node& get_top_node();
+
+      // смещение итератора вертикальных пар на offset
+      Vertical_pair_nodes_iterator& move(const std::array<int, NUMBER_OF_DIMENSIONS>& offset);
+      void positive_move(Vector_of_indexes offset);
+
+      // Перегруженный оператор смещения по последней координате пространства или смещение по линейному массиву узлов
+      Vertical_pair_nodes_iterator& operator += (Vertical_pair_nodes_iterator& iterator, int offset);
+
+      Node* _bottom_node;
+      Node* _top_node;
+
+      // Номер элемента вектора (узлов)
+      size_t _index;
+    };
+
   private:
+
+    // вектор размеров расчетной области
     Vector _sizes;
+    // вектор размеров в узлах (количество узлов на ось)
     std::array<size_t, NUMBER_OF_DIMENSIONS> _sizes_in_nodes;
+    // вектор размеров в ячейках
+    std::array<size_t, NUMBER_OF_DIMENSIONS> _sizes_in_cells;
+    // вектор узлов
     std::vector<Node> _nodes;
+    // вектор ячеек
     std::vector<Node> _cells;
 
 };
